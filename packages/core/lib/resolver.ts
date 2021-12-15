7a076d41c8485d083b495e3cf06f1cb8e164f0b7
Nate Moore <nate@skypack.dev>
2021-12-14 22:22:19 -0600

    fix(#15): fix parallelism issue with remote cache

--- packages/core/lib/resolver.ts
+++ packages/core/lib/resolver.ts
@@ -7,28 +7,30 @@ export default async function get(pack: string, name: string) {
   const url = new URL(`./${pack}/${name}`, baseURL).toString();
   // Handle in-flight requests
   if (requests.has(url)) {
-    await requests.get(url);
-    return fetchCache.get(url);
+    return await requests.get(url);
   }
-
   if (fetchCache.has(url)) {
     return fetchCache.get(url);
   }
-  let request = fetch(url);
-  requests.set(url, request);
-  const res = await request;
-  if (!res.ok) {
-    throw new Error(await res.text());
-  }
-  const contentType = res.headers.get("Content-Type");
-  if (!contentType.includes("svg")) {
-    throw new Error(`[astro-icon] Unable to load "${name}" because it did not resolve to an SVG!
+
+  let request = async () => {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(await res.text());
+    }
+    const contentType = res.headers.get("Content-Type");
+    if (!contentType.includes("svg")) {
+      throw new Error(`[astro-icon] Unable to load "${name}" because it did not resolve to an SVG!
 
 Recieved the following "Content-Type":
 ${contentType}`);
+    }
+    const svg = await res.text();
+    fetchCache.set(url, svg);
+    requests.delete(url);
+    return svg;
   }
-  const svg = await res.text();
-  fetchCache.set(url, svg);
-  requests.delete(url);
-  return svg;
+  let promise = request();
+  requests.set(url, promise);
+  return await promise;
 }
