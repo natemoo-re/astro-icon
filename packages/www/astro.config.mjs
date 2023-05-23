a064d2b9d2c11d8fd7ac48271b06d948fd174ea0
Michael Stramel <michael.stramel@koyfin.com>
2023-05-23 14:11:23 -0500

    chore: update website to use latest code

---
+++ packages/www/astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://github.com',
+    base: '/natemoo-re/astro-icon',
+    integrations: [
+        icon({
+            include: {
+              pixelarticons: ['external-link'],
+            }
+        })
+    ]
+});
