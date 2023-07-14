ae27b7398c6e0def36198d88f9eea183001e30e2
Charlie Hadden <charlie@hadden.me>
2023-07-14 11:01:25 +0100

    Rough prototype of transform options

--- packages/core/components/cache.ts
+++ packages/core/components/cache.ts
@@ -1,1 +1,12 @@
 export const cache = new WeakMap<Request, Map<string, number>>();
+
+export interface CacheKeyFields {
+  name: string
+  hFlip?: boolean;
+  vFlip?: boolean;
+  rotate?: number;
+}
+
+export function cacheKey(fields: CacheKeyFields) {
+  return `${fields.name}:${fields.hFlip}:${fields.vFlip}:${fields.rotate}`
+}
