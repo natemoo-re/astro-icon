ee730342231c6b4b682b1255053d27cbde8c37a7
liruifengv <liruifeng1024@gmail.com>
2023-09-01 11:20:10 +0800

    fix type import

--- packages/core/lib/utils.ts
+++ packages/core/lib/utils.ts
@@ -1,5 +1,5 @@
 import { SPRITESHEET_NAMESPACE } from "./constants";
-import { Props, Optimize } from "./Props";
+import { type Props, type Optimize } from "./Props";
 import getFromService from "./resolver";
 import { optimize as optimizeSVGNative } from "svgo";
 
