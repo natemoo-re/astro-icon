e9f35eb406c81ae3db8f09b1244c5daba4ade3fd
Nate Moore <nate@astro.build>
2023-12-22 09:31:37 -0600

    feat: scaffold docs

--- packages/www/astro.config.mjs
+++ packages/www/astro.config.mjs
@@ -1,16 +1,36 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import icon from 'astro-icon';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com',
-    base: '/natemoo-re/astro-icon',
-    integrations: [
-        icon({
-            include: {
-              pixelarticons: ['external-link'],
-            }
-        })
-    ]
+	integrations: [
+		starlight({
+			title: 'Astro Icon',
+			social: {
+				github: 'https://github.com/natemoo-re/astro-icon',
+			},
+			sidebar: [
+				{
+					label: 'Start Here',
+					items: [
+						{ label: 'Getting Started', link: '/getting-started/' },
+						{ label: 'Upgrade to v1', link: '/guides/upgrade/v1' },
+					],
+				},
+				{
+					label: 'Guides',
+					items: [
+						{ label: 'Icons', link: '/guides/icons/' },
+						{ label: 'Components', link: '/guides/components/' },
+						{ label: 'Styling', link: '/guides/styling/' },
+						{ label: 'Server Rendering', link: '/guides/server-rendering/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
 });
--- packages/www/src/env.d.ts
+++ packages/www/src/env.d.ts
@@ -1,3 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference path="../.astro/icon.d.ts" />
 /// <reference types="astro/client" />
