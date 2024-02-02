542d85528a3a5f77701d361b4716ef1aa04b3d09
Nate Moore <nate@astro.build>
2024-02-02 13:48:34 -0600

    feat: add support for passthrough attributes

---
+++ packages/core/components/utils.ts
@@ -0,0 +1,16 @@
+export const cache = new WeakMap<Request, Map<string, number>>();
+
+// TODO: are there other attributes we should pass through?
+function isPassthroughAttribute(key: string) {
+    if (key.startsWith('stroke-')) return true;
+}
+
+// TODO: not the most robust, but it works
+export function applyPassthroughAttributes(body: string, props: Record<string, any>) {
+    for (const [key, value] of Object.entries(props)) {
+        if (!isPassthroughAttribute(key)) continue;
+        if (!body.includes(`${key}="`)) continue;
+        body = body.replace(new RegExp(`${key}="[^"]"`, 'g'), `${key}="${value}"`)
+    }
+    return body;
+}
