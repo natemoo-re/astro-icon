59346ce993e97ff327390ad59e76a3ce61bf8491
Michael Stramel <m.stramel89@gmail.com>
2024-12-01 23:30:44 -0600

    bump to beta.11

--- demo/astro.config.ts
+++ demo/astro.config.ts
@@ -3,5 +3,10 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    svg: {
+      mode: "sprite",
+    },
+  },
   integrations: [icon()],
 });
